5e1d0e5b1fc6f0c66f43b566412dac24552dd94b
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-01-28 12:56:52 +0200

    fix: get lessons based on subject

--- src/controllers/lesson/get-lessons.ts
+++ src/controllers/lesson/get-lessons.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { Lesson } from "../../models/lesson.model.js";
 import { getLessonsService } from "../../services/lesson/get-lessons.js";
+import { BadRequestError } from "../../errors/BadRequest.js";
 
 export const getLessons = async (req: Request, res: Response) => {
     try {
-        const lessons: Lesson[] = await getLessonsService()
+        const {id} = req.query
+        if(!id || typeof(id) !== "string") throw BadRequestError("Subject id missing")
+        const lessons: Lesson[] = await getLessonsService(id)
         res.status(200).send(lessons)
     } catch(e) {
-        res.status(500).send(e)
+        res.status(500).send({message: e.message})
     }
 }
--- src/services/lesson/get-lessons.ts
+++ src/services/lesson/get-lessons.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { Lesson } from "../../models/lesson.model.js";
+import { BadRequestError } from "../../errors/BadRequest.js";
 
 const prisma = new PrismaClient()
-export const getLessonsService = async (): Promise<Array<Lesson>> => {
+export const getLessonsService = async (id:string): Promise<Array<Lesson>> => {
     try {
         await prisma.$connect()
-        const lessons = await prisma.lesson.findMany()
+        const lessons = await prisma.lesson.findMany({
+            where: {
+                subjectId: id
+            }
+        })
+        if(!lessons) throw BadRequestError("Lessons not found")
         return lessons
     } catch(e) {
         throw e
