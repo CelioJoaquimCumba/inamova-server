fc1f841f90f1cf34035d5752ad806dee0af8ea6e
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-02-20 11:03:24 +0200

    fix: password validation to include especial symbols such as . ? and so on

--- src/validations/user/passwordValidation.ts
+++ src/validations/user/passwordValidation.ts
@@ -1,6 +1,6 @@
 export const PasswordValidation = (password) => {
     // Regular expression pattern for password validation
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/;
+    const re = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]/;
 
     // Test if the provided password matches the pattern
     return re.test(password);
